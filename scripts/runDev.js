353e046df2a26d4c9e7572517b75eb56c36208a6
阳九 <871921282@qq.com>
2023-05-26 15:42:51 +0800

    更新devServer运行脚本

--- scripts/runDev.js
+++ scripts/runDev.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-const DevServer = require('../lib/devServer/DevServer')
+const WebpackDevServer = require('../lib/devServer/DevServer')
 const createCompiler = require('../lib/core/webpack')
 
-
 const webpackCompiler = createCompiler()
-const devServe = new DevServer(webpackCompiler)
+
+const devServe = new WebpackDevServer(webpackCompiler)
+
+devServe.start()
 
 
-devServe.run()
